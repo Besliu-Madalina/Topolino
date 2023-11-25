50655c1020bf57684def3b09042da090067f2284
Besliu-Madalina <104641549+Besliu-Madalina@users.noreply.github.com>
2023-11-25 20:54:43 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 new SimpleLightbox({ elements: '.container-galerie a' });
-new SimpleLightbox({ elements: '.serv-galerie a'})
+new SimpleLightbox({ elements: '.serv-galerie a' })
 
 AOS.init({
     duration: 900,
@@ -18,6 +18,72 @@ function mobile() {
         x.className = "navbar";
     }
 }
+
+function onload() {
+    getYear();
+    console.log('page loaded');
+    // setScrollListener();
+}
+
+function getYear() {
+    var d = new Date(),
+        date = d.getDate(),
+        mon = d.getMonth(),
+        yr = d.getFullYear();
+    // var days = ["Duminică", "Luni", "Marți", "Miercuri", "Joi", "Vineri", "Sâmbătă"];
+    // document.getElementById("year").innerHTML = ((date < 10) ? '0' + date : date) + "/" +
+    document.getElementById("year").innerHTML = ('0' + date).slice(-2) + "/" +
+        // (((mon + 1) < 10) ? '0' + (mon + 1) : (mon + 1)) + "/" + yr + " " + days[d.getDay()];
+        ('0' + (mon + 1)).slice(-2) + "/" + yr;
+}
+
+function send2() {
+
+    if (document.getElementById("rezervare").checkValidity() == false) {
+        document.getElementById("rezervare").reportValidity();
+        return;
+    }
+
+    var datal = {
+        nume: document.getElementById("nume").value,
+        email: document.getElementById("email").value,
+        nr_telefon: document.getElementById("nr_telefon").value,
+        data: document.getElementById("data").value,
+        ora_sosirii: document.getElementById("ora_sosirii").value,
+        ora_plecarii: document.getElementById("ora_plecarii").value,
+        copii: document.getElementById("copii").value
+    };
+
+    emailjs.send("service_rguiedf", "template_8y9hhja", datal)
+        .then(function (raspuns) {
+            btn.value = 'SEND MESSAGE';
+            alert("Mesajul a fost transmis. Operatorul o să vă contacteze în decurs de câteva ore.")
+            document.getElementById("rezervare").reset();
+        }, function (error) {
+            alert("Eroare la transmitere. Contactati programatorul.")
+        })
+}
+
+// const btn = document.getElementById('button');
+
+// document.getElementById('form')
+//  .addEventListener('submit', function(event) {
+//    event.preventDefault();
+
+//    btn.value = 'Sending...';
+
+//    const serviceID = 'service_rguiedf';
+//    const templateID = 'template_8y9hhja';
+
+//    emailjs.sendForm(serviceID, templateID, this)
+//     .then(() => {
+//       btn.value = 'Rezervează!';
+//       alert('Sent!');
+//     }, (err) => {
+//       btn.value = 'Rezervează!';
+//       alert(JSON.stringify(err));
+//     });
+// });
 
 function send() {
 
